22eeb3008b574e20398bfdd18524d3c9680042ac
Kaung Myat Aung (Eddie) <36400660+eddiekaung@users.noreply.github.com>
2021-09-18 09:17:48 -0700

    Added stellar endpoints (createIssuer, checkAccount)

---
+++ stellar.js
@@ -0,0 +1,84 @@
+const { NotFoundError } = require('stellar-sdk');
+const StellarSdk = require('stellar-sdk');
+
+const errMessages = {
+    privatePublicKey: "Private key and public key don't match",
+    insufficientBalance: "Insufficient balance",
+}
+
+const mintNFT = async (creatorPublicKey, creatorPrivateKey, NFTName) => {
+    var server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+    var creator = StellarSdk.Keypair.fromSecret(creatorPrivateKey)
+    var err = await checkCreatorAccount(creatorPublicKey, creatorPrivateKey)
+    if (err != '') {
+        return [null, err]
+    }
+    const { transaction, NFT, issuerPrivateKey, issuerPublicKey } = await createIssuer(creator.publicKey(), NFTName);
+    return [{ transaction, NFT, issuerPrivateKey, issuerPublicKey }, '']
+
+}
+
+const checkCreatorAccount = async (creatorPublicKey, creatorPrivateKey) => {
+    var account = ''
+    const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+    const creator = StellarSdk.Keypair.fromSecret(creatorPrivateKey)
+    if (creator.publicKey() != creatorPublicKey) {
+        console.log(creator.publicKey())
+        console.log(creatorPublicKey)
+        return errMessages.privatePublicKey
+    }
+    try {
+        account = await server.loadAccount(creatorPublicKey);
+    } catch (err) {
+        return err.response.detail
+    }
+    const balance = account.balances.find(balance => balance.asset_type == 'native')
+    if(balance < 2){
+        return errMessages.insufficientBalance
+    }
+    return ''
+}
+
+const createIssuer = async (creatorPublicKey, NFTName) => {
+    var server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+    const issuer = StellarSdk.Keypair.random()
+    const createAccount = ''
+    try{
+        creatorAccount = await server.loadAccount(creatorPublicKey)
+    } catch (err) {
+        console.log(err)
+        return
+    }
+    const NFT = new StellarSdk.Asset(NFTName, issuer.publicKey())
+    const fee = StellarSdk.BASE_FEE;
+    const networkPassphrase = StellarSdk.Networks.TESTNET;
+    const transaction = new StellarSdk.TransactionBuilder(creatorAccount, { fee, networkPassphrase })
+        .addOperation(
+            StellarSdk.Operation.createAccount({
+                destination: issuer.publicKey(),
+                startingBalance: '1'
+            })
+        )
+        .addOperation(
+            StellarSdk.Operation.changeTrust({
+                asset: NFT,
+                limit: '1',
+            })
+        )
+        .setTimeout(500)
+        .build();
+    console.log(transaction)
+    return {
+        transaction,
+        NFT,
+        issuerPrivateKey: issuer.secret(),
+        issuerPublicKey: issuer.publicKey(),
+    }
+}
+
+// createIssuer("GC3FP3NOXKZAVWSYNV4FBADWN5UBWFZSPVUYBY4GVK5ZZ333D4FOGAIA", "testingagain").then((data) => console.log(data))
+mintNFT("GC3FP3NOXKZAVWSYNV4FBADWN5UBWFZSPVUYBY4GVK5ZZ333D4FOGAIAs", "SBYYLANBRT3EWLDEHUBDHRXU5CKLOOIQVOY7ECPG2FEUPFFDR7E45O7F", "testingagain")
+    .then(([res, err]) => {
+        console.log("response", res)
+        console.log("error", err)
+    })
