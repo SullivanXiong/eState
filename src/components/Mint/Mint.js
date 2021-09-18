e3dda2f5635599a03b8d937649011fd5f48a862e
SullivanXiong <sxiong5307@gmail.com>
2021-09-18 14:34:38 -0700

    Finished Mint Page and File Upload/IPFS Logic

--- src/components/Mint/Mint.js
+++ src/components/Mint/Mint.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import MintForm from './MintForm';
 import './Mint.css'
 
@@ -13,12 +14,22 @@ function Mint() {
         card: {
             position: 'relative',
             margin: 'auto',
-            top: '20vh'
+            'margin-top': '33%'
         },
         previewHeader: {
             'margin-top': '2.5vh' 
+        },
+        formHeader: {
+            'padding-top': '1vh',
+            'padding-left': '1.5vw'
+        },
+        defaultColor: {
+            color: 'grey'
         }
     }
+    
+    const [estateName, setEstateName] = useState(undefined);
+    const [adress, setAddress] = useState(undefined);
 
     return (
         <div className="Mint">
@@ -35,13 +46,26 @@ function Mint() {
                             alt="green iguana"
                         />
                         <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                ESTATE
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                            </Typography>
+                            {!estateName && (
+                                <Typography gutterBottom variant="h5" component="div" style={style.defaultColor}>
+                                    Estate Name
+                                </Typography>
+                            )}
+                            {estateName && (
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {estateName}
+                                </Typography>
+                            )}
+                            {!adress && (
+                                <Typography variant="body2" color="text.secondary" style={style.defaultColor}>
+                                    123 Address St
+                                </Typography>
+                            )}
+                            {adress && (
+                                <Typography variant="body2" color="text.secondary" style={style.defaultColor}>
+                                    {adress}
+                                </Typography>
+                            )}
                         </CardContent>
                         <CardActions>
                             <Button size="small">Share</Button>
@@ -51,7 +75,12 @@ function Mint() {
                 </div>
             </div>
             <div className="MintForm">
-                <MintForm></MintForm>
+                <Typography variant="h4" style={style.formHeader}>
+                    Mint a new Estate
+                </Typography>
+                <Divider>
+                </Divider>
+                <MintForm setEstateName={setEstateName} setAddress={setAddress}></MintForm>
             </div>
         </div>
     );
